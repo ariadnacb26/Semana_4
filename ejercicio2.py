8859c59f8a7f293a4b290785755a9cfc8158f995
ariadnacb26 <ariadnacb@uamv.edu.ni>
2025-04-08 08:41:28 -0600

    Ejercicios practicos

---
+++ ejercicio2.py
@@ -0,0 +1,9 @@
+cantidad = 10
+precio = 130
+total = cantidad * precio
+def ftexto(texto):
+    return f"(texto:<10)"
+
+print(f"{cantidad:>10} {cantidad:>6}")
+print(f"{ftexto(Precio:)} {precio:>6}")
+print(f"{ftexto(Total:)} {total:>6}")
---
+++ ejercicio4.py
@@ -0,0 +1,6 @@
+#operaciones aritmeticas
+
+print(5 + 3) #suma
+print(5 - 3) #resta
+print(5 * 3) #multiplicacion 
+print(5 / 3) #multiplicacion 
